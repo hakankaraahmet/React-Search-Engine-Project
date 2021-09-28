2ad20f1cad4da8a2b10ca0dd14e7e2c004123f5a
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-28 16:40:58 +0300

    Info component is added into MainPage as well as Navbar and Form components

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Form from "../components/Form";
+import Info from "../components/Info";
+
 
 const MainPage = () => {
   return (
-    <div>
+    <div className="mainpage">
       <Navbar />
       <Form />
+      <Info />
     </div>
   );
 };
