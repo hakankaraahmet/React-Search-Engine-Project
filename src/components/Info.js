f39f6c13ddf79749f61a2979af674a2fd4a6b7f3
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-28 17:13:41 +0300

    taken data with context and exposured to map function and sent all items to Card component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { engineContext } from '../context/engineContext';
+import Card from "./Card";
+
 
 const Info = () => {
+    
+    const {data} = useContext(engineContext);
+    console.log(data)
+
     return (
-        <div>
-            asd
+        <div className="">
+            {data.map((item,index)=> (
+                <Card key={index} item={item} />
+            ))}
         </div>
     )
 }
