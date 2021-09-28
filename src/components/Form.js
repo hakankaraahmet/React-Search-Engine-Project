6b2dbe0035ef7bd088e7a4178278bbd38d23044b
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-28 16:41:21 +0300

    Search-bar section is added and will be modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="search-bar input-group">
+      <div className="form-outline d-flex w-50">
+        <input
+          type="search"
+          id="form1 "
+          className="form-control me-3"
+          placeholder="Enter name/surname"
+        />
+        <button type="button" className="btn btn-primary">
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
