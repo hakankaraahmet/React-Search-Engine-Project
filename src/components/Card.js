80d5b173ff43f017958658f30daf958da3d2c07c
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-30 00:48:00 +0300

    Cards are grouped

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,16 @@
 import React from "react";
-import { useContext } from 'react';
-import { engineContext } from '../context/engineContext';
-
-
 
 const Card = ({item}) => {
-
-    const data = useContext(engineContext);
 
   return (
     <div className="card col-md-4 mx-3 my-5" style={{width: "25rem"}}>
       <div className="card-body">
-        <h5 className="card-title">{item[0]} </h5>
+        <h5 className="card-title"> {item[0]} </h5>
         <p className="card-text"> {item[1]}</p>
         <p className="card-text"> {item[2]}</p>
         <p className="card-text"> {item[3]}</p>
-        <p className="card-text">{item[4]}</p>
-        <p className="card-text">{item[5]}</p>
+        <p className="card-text"> {item[4]}</p>
+        <p className="card-text"> {item[5]}</p>
       </div>
     </div>
   );
