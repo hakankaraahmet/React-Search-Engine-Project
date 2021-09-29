eed1147a100eb4804777cb29be1602b6022213c3
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-29 14:17:17 +0300

    data is modified and sent via context

--- src/App.js
+++ src/App.js
@@ -11,12 +11,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   const [data, setData] = useState(initialStates);
-  const [cols, setCols] = useState(initialStates);
 
 
   return (
     <Router>
-      <engineContext.Provider value={data,cols}>
+      <engineContext.Provider value={data}>
         <Switch>
           <Route path="/resultpage" component={ResultPage} />
           <Route path="/addpage" component={AddPage} />
