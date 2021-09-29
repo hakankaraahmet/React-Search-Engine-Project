6e86ddabcc14f0b8857ba3df88e73db7110440ec
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-29 14:17:47 +0300

    Info section is deleted and the contents are moved to Form component

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Form from "../components/Form";
-import Info from "../components/Info";
-
 
 const MainPage = () => {
   return (
     <div className="mainpage">
       <Navbar />
       <Form />
-      <Info />
     </div>
   );
 };
