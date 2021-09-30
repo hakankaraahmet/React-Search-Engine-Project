9109d88049ab786c0dbbd8212eb4f34763775def
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-30 14:07:19 +0300

    the output is taken with props and displayed the screen

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router";
 
+const ResultPage = (props) => {
+  const history = useHistory();
+  const goMain = () => {
+    history.push("/");
+  };
 
-const ResultPage = () => {
- 
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="d-flex flex-column">
+      <div className="d-flex justify-content-center">
+        <button
+          type="button"
+          className="logobutton btn btn-outline-primary btn-lg mt-5 p-3 "
+          onClick={goMain}
+        >
+          <i className="fas fa-arrow-left"></i> Return To Main Page
+        </button>
+      </div>
+
+      <div className="output col-md-10">
+          {props.location.output}
+     </div>
+    </div>
+  );
+};
 
 export default ResultPage;
