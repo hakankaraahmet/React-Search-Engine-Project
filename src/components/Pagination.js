ea67f68cfc91dbbe3f4b8c7e06654138a0ad7ec5
hakankaraahmet <karaahmethkn@gmail.com>
2021-10-01 16:32:02 +0300

    modified pagination.js with minor changes

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = ({cards}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Pagination = ({ paginate, output, cardPerPage }) => {
+  const pageNumbers = [];
 
-export default Pagination
+  for (let i = 1; i <= Math.ceil(output.length / cardPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination d-flex justify-content-center mt-3">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <a
+              onClick={() => paginate(number)}
+              href="#"
+              className="page-link bg-secondary text-light h5 m-1"
+            >
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
